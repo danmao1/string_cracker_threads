3dea5b5e0dc023b98ed4ae12de62f734358777e8
Daniel Mao <danielmao@MBP-de-Daniel.lan>
2021-10-22 12:31:42 -0500

    needs checking

--- cracker.c
+++ cracker.c
@@ -6,20 +6,23 @@
 #include <crypt.h>
 
 
-char password[8];
+
 char var;
 char salt[3];
 char* target;
-pthread_mutex_t lock;
+int size;
+char letter_to_check='a';
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
 
 void* cracker(void* args){
     struct crypt_data data;
     data.initialized = 0;
-    int size=(int)args;
-    pthread_mutex_lock(&lock);
-    for (int i=1;i<size;i++){
+    char password[8];
+    
+    
+    for (int i=0;i<size;i++){
         password[i]='a';
     }
     password[size]='\0';
@@ -27,9 +30,20 @@ void* cracker(void* args){
         for(int i=1;i<size-1;i++){
             if(password[i]=='{'){
                 password[i]='a';
-                var=password[i-1];
-                var++;
-                password[i-1]=var;
+                if(i==1){
+                    pthread_mutex_lock(&mutex);
+                    letter_to_check++;
+                    password[0]=letter_to_check;
+                    pthread_mutex_unlock(&mutex);
+        
+
+                }
+                else{
+                    var=password[i-1];
+                    var++;
+                    password[i-1]=var;
+                }
+                
             }
 
         }
@@ -37,6 +51,7 @@ void* cracker(void* args){
             password[size-1]=c;
             if(strcmp(crypt_r(password,salt,&data),target)==0){
                 printf("This is password: %s\n",password);
+                exit(0);
                 return NULL;
             }
 
@@ -49,7 +64,6 @@ void* cracker(void* args){
 
 
     }
-    pthread_mutex_unlock(&lock);
 
 
 
@@ -63,45 +77,35 @@ int main(int argc, char* argv[]){
     num_threads=atoi(argv[1]);
     pthread_t tid[num_threads];
 
-    int keySize;
-    char first;
-    keySize=atoi(argv[2]);
+   
+    
+    size=atoi(argv[2]);
     target=argv[3];
 
     salt[0]=target[0];
     salt[1]=target[1];
     salt[2]='\0';
-    int sep=26/num_threads;
-    int sum=0;
-    int mutex=pthread_mutex_init(&lock,NULL);
-    for(int i=0;i<num_threads;i++){
-        thread_sep[i]=sum;
-        sum+=sep;
-
-    }
+   
     
     
     
     printf("this is the target: %s\n",target);
     printf("this is the salt: %s\n",salt);
     for(int i=0;i<num_threads;i++){
-        first='a';
-        for(int j=0;j<thread_sep[i];j++){
-            first++;
-        }
-        password[0]=first;
-        int creating_thread=pthread_create(&tid[i],NULL,cracker,(void*)keySize);
+        
+        int creating_thread=pthread_create(&tid[i],NULL,cracker,NULL);
         if(creating_thread!=0){
             printf("Could not create thread\n");
         }
     }
     for (int i=0;i<num_threads;i++){
+
+        
         int creating_join=pthread_join(tid[i],NULL);
         if(creating_join!=0){
             printf("Could not create join\n");
         }
 
     }
-    pthread_mutex_destroy(&lock);
     return 0;
 }
