0b88a6a05931f9690bf1fe76fe44e40902e694f8
Daniel Mao <danielmao@MacBook-Pro-de-Daniel.local>
2021-10-25 19:58:57 -0500

    Submission

--- cracker.c
+++ cracker.c
@@ -11,38 +11,56 @@ char var;
 char salt[3];
 char* target;
 int size;
-char letter_to_check='a';
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
+char letter_to_check='a';
 
 
 void* cracker(void* args){
+    
+    int count=0;
+    
     struct crypt_data data;
     data.initialized = 0;
-    char password[8];
-    
+    char password[8]="\0\0\0\0\0\0\0\0";
+    char ending[8]="\0\0\0\0\0\0\0\0";
     
     for (int i=0;i<size;i++){
         password[i]='a';
     }
-    password[size]='\0';
-    while(password[0]!='{'){
+    letter_to_check=password[0];
+    for (int i=0;i<size;i++){
+        ending[i]='z';
+    }
+    
+    
+
+    while(strncmp(ending,password,size)!=0 && 'a'<= password[0]<='z'){
         for(int i=1;i<size-1;i++){
             if(password[i]=='{'){
                 password[i]='a';
+                
                 if(i==1){
-                    pthread_mutex_lock(&mutex);
-                    letter_to_check++;
-                    password[0]=letter_to_check;
-                    pthread_mutex_unlock(&mutex);
-        
-
+                    
+                   
+                    
+                    if('a'<=password[0]<'z'){
+                        pthread_mutex_lock(&mutex);
+                        letter_to_check++;
+                        password[0]=letter_to_check;
+                        pthread_mutex_unlock(&mutex);
+                    }
+                    else{
+                        return NULL;
+                    }
+                    
                 }
                 else{
                     var=password[i-1];
                     var++;
                     password[i-1]=var;
                 }
+
+
                 
             }
 
@@ -50,8 +68,10 @@ void* cracker(void* args){
         for (char c='a';c<='z';c++){
             password[size-1]=c;
             
+            count++;
             if(strcmp(crypt_r(password,salt,&data),target)==0){
                 printf("This is password: %s\n",password);
+                printf("this is num of hashes: %d\n", count);
                 exit(0);
                 return NULL;
             }
@@ -61,11 +81,24 @@ void* cracker(void* args){
         var++;
         password[size-2]=var;
 
+    }
 
+    for(char c='a';c<='z';c++){
+        ending[size-1]=c;
+        
+        count++;
+        if(strcmp(crypt_r(ending,salt,&data),target)==0){
+            
+            printf("This is password: %s\n",password);
+            printf("this is num of hashes: %d\n", count);
+            exit(0);
+            return NULL;
+            
+        }
 
 
     }
-
+    printf("this is num of hashes: %d\n", count);
 
 
 
@@ -93,6 +126,7 @@ int main(int argc, char* argv[]){
     printf("this is the target: %s\n",target);
     printf("this is the salt: %s\n",salt);
     while(size!=0){
+    
         for(int i=0;i<num_threads;i++){
             
             int creating_thread=pthread_create(&tid[i],NULL,cracker,NULL);
@@ -108,8 +142,8 @@ int main(int argc, char* argv[]){
             }
 
         }
+        
         size--;
     }
-    
     return 0;
 }
